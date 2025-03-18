0472d9902ffbae914c1203f93e3778d7c3917f90
infoziantappwrite <infoziantappwrite@gmail.com>
2025-03-18 16:27:03 +0530

    Revert "router resolve"

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRoute as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Login from './pages/Login.jsx';
